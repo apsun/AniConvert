22e9edfc065feab6fbd26e52165e528f7227de78
Andrew Sun <youlosethegame@live.com>
2015-07-30 10:09:42 +0800

    Code refactor

--- main.py
+++ main.py
@@ -15,23 +15,6 @@ import errno
 import os
 import re
 import subprocess
-import textwrap
-
-# Use raw_input() in Python 2
-try:
-    input = raw_input
-except NameError:
-    pass
-
-# textwrap.indent() for Python pre-3.3
-try:
-    indent_text = textwrap.indent
-except AttributeError:
-    def indent_text(text, prefix):
-        def prefixed_lines():
-            for line in text.splitlines(True):
-                yield (prefix + line if line.strip() else line)
-        return "".join(prefixed_lines())
 
 ###############################################################
 # Configuration values, no corresponding command-line args
@@ -93,12 +76,11 @@ HANDBRAKE_ARGS = """
 # in the most preferable language, the program will check 
 # the second most preferable language, and so on. This value 
 # should use the iso639-2 (3 letter) language code format.
-# On the command line, specify as "--audio-languages jpn,eng"
+# On the command line, specify as "-A jpn,eng"
 AUDIO_LANGUAGES = ["jpn", "eng"]
 
 # This is the same as the preferred audio languages, but 
-# for subtitles. On the command line, specify as 
-# "--subtitle-languages eng"
+# for subtitles. On the command line, specify as "-S eng"
 SUBTITLE_LANGUAGES = ["eng"]
 
 # What to do when the destination file already exists. Can be 
@@ -130,6 +112,13 @@ RECURSIVE_SEARCH = False
 CHECK_ALL_FILES = False
 
 
+# Use raw_input() in Python 2
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
 class HandBrakeAudioInfo:
     pattern1 = re.compile(r"(\d+), (.+) \(iso639-2: (\S+)\)")
     pattern2 = re.compile(r"(\d+), (.+) \(iso639-2: (\S+)\), (\d+)Hz, (\d+)bps")
@@ -152,15 +141,16 @@ class HandBrakeAudioInfo:
             self.bit_rate = None
 
     def __str__(self):
-        format_str = textwrap.dedent("""\
-            Description: {description}
-            Language code: {language_code}""")
-
+        format_str = (
+            "Description: {description}\n"
+            "Language code: {language_code}"
+        )
         if self.sample_rate and self.bit_rate:
-            format_str += textwrap.dedent("""
-                Sample rate: {sample_rate}Hz
-                Bit rate: {bit_rate}bps""")
-
+            format_str += (
+                "\n"
+                "Sample rate: {sample_rate}Hz\n"
+                "Bit rate: {bit_rate}bps"
+            )
         return format_str.format(**self.__dict__)
 
     def __repr__(self):
@@ -206,11 +196,12 @@ class HandBrakeSubtitleInfo:
         self.source = match.group(5)
 
     def __str__(self):
-        return textwrap.dedent("""\
-            Language: {language}
-            Language code: {language_code}
-            Format: {format}
-            Source: {source}""").format(**self.__dict__)
+        return (
+            "Language: {language}\n"
+            "Language code: {language_code}\n"
+            "Format: {format}\n"
+            "Source: {source}"
+        ).format(**self.__dict__)
 
     def __repr__(self):
         format_str = "{index}, {language} (iso639-2: {language_code}) ({format})({source})"
@@ -236,6 +227,11 @@ class HandBrakeSubtitleInfo:
             self.format == other.format and 
             self.source == other.source
         )
+
+
+def indent_text(text, prefix):
+    lines = text.splitlines()
+    return "\n".join(prefix + line for line in lines)
 
 
 def get_videos_in_dir(path, recursive):
@@ -325,20 +321,21 @@ def filter_tracks_by_language(track_list, preferred_languages):
     return track_list
 
 
-def print_audio_track_header(track_list):
+def prompt_select_audio_track(track_list):
     for track in track_list:
         print("Audio track #{0}:".format(track.index))
         print(indent_text(str(track), "    "))
+    return prompt_select_track(track_list)
 
 
-def print_subtitle_track_header(track_list):
+def prompt_select_subtitle_track(track_list):
     for track in track_list:
         print("Subtitle track #{0}:".format(track.index))
         print(indent_text(str(track), "    "))
+    return prompt_select_track(track_list)
 
 
-def prompt_select_track(track_list, header_printer):
-    header_printer(track_list)
+def prompt_select_track(track_list):
     while True:
         print("Choose a track #: ", end="")
         input_str = input()
@@ -367,12 +364,15 @@ def prompt_overwrite_file(filename):
             print("Enter either 'y' or 'n'!")
 
 
-def select_best_track(track_list, preferred_languages, header_printer):
+def select_best_track(track_list, preferred_languages, prompt_func):
     filtered_tracks = filter_tracks_by_language(track_list, preferred_languages)
     if len(filtered_tracks) == 1:
         return filtered_tracks[0]
+    elif len(filtered_tracks) > 1:
+        return prompt_func(filtered_tracks)
     else:
-        return prompt_select_track(filtered_tracks, header_printer)
+        # TODO: Handle no tracks
+        raise Exception("No tracks found")
 
 
 def get_track_by_index(track_list, track_index):
@@ -414,7 +414,7 @@ def parse_output_dimensions(value):
         return (1280, 720)
     match = re.match("^(\d+)x(\d+)$", value_lower)
     if not match:
-        raise argparse.ArgumentTypeError(value + " is not a valid video dimension value")
+        raise argparse.ArgumentTypeError(repr(value) + " is not a valid video dimension value")
     width = int(match.group(1))
     height = int(match.group(2))
     return (width, height)
@@ -423,7 +423,7 @@ def parse_output_dimensions(value):
 def parse_duplicate_action(value):
     value_lower = value.lower()
     if value_lower not in {"prompt", "skip", "overwrite"}:
-        raise argparse.ArgumentTypeError(value + " is not a valid duplicate action")
+        raise argparse.ArgumentTypeError(repr(value) + " is not a valid duplicate action")
     return value_lower
 
 
@@ -431,7 +431,7 @@ def parse_language_arg(value):
     language_list = value.split(",")
     for language in language_list:
         if len(language) != 3 or not language.isalpha():
-            raise argparse.ArgumentTypeError(language + " is not a valid iso639-2 code")
+            raise argparse.ArgumentTypeError(repr(language) + " is not a valid iso639-2 code")
         # TODO: Maybe add some real validation here?
     return language_list
 
@@ -460,19 +460,22 @@ def main():
         if args.check_all_files:
             check_video_files()
 
+        # TODO: Change this according to args.check_all_files
         audio_tracks, subtitle_tracks = get_track_info(os.path.join(subdir, filenames[0]))
 
         if args.audio_index is not None:
+            # TODO: Handle track index not found
             audio_track = get_track_by_index(audio_tracks, args.audio_index)
         else:
             audio_track = select_best_track(audio_tracks, 
-                args.audio_languages, print_audio_track_header)
+                args.audio_languages, prompt_select_audio_track)
 
         if args.subtitle_index is not None:
+            # TODO: Handle track index not found
             subtitle_track = get_track_by_index(subtitle_tracks, args.subtitle_index)
         else:
             subtitle_track = select_best_track(subtitle_tracks, 
-                args.subtitle_languages, print_subtitle_track_header)
+                args.subtitle_languages, prompt_select_subtitle_track)
 
         for filename in filenames:
             file_path = os.path.join(subdir, filename)
